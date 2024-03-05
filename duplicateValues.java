32733820879bdc74e71184687af43904f023b793
AmnaSaher31 <161111441+AmnaSaher31@users.noreply.github.com>
2024-03-05 15:52:45 +0530

    Update duplicateelementsjava

--- duplicateValues.java
+++ duplicateValues.java
@@ -1,27 +1,31 @@
-package week5;
+package Week7;
 import java.util.Scanner;
-public class duplicateValues {
-	    public static void main(String[] args) {
-	        Scanner scanner = new Scanner(System.in);
-	        System.out.println("Enter the size of the array: ");
-	        int size = scanner.nextInt();
-	        int[] array = new int[size];
-	        System.out.println("Enter the elements of the array: ");
-	        for (int i = 0; i < size; i++) {
-	            array[i] = scanner.nextInt();
-	        }
-	        System.out.println("Does the array contain any duplicate elements? " + hasDuplicates(array));
-	    }
-
-	    public static boolean hasDuplicates(int[] array) {
-	        for (int i = 0; i < array.length; i++) {
-	            for (int j = i + 1; j < array.length; j++) {
-	                if (array[i] == array[j]) {
-	                    return true;
-	                }
-	            }
-	        }
-	        return false;
-	    }
+public class DuplicateinArray {
+	public static void main(String[] args) {
+		int array[] = new int[10]; boolean isDuplicate=false;
+		Scanner inScanner= new Scanner (System.in);
+		System.out.println("enter array size"); int size=
+             inScanner.nextInt();    
+		System.out.println("enter elements one by one");
+		for (int i=0; i<size; i++) 
+			array[i]= inScanner.nextInt();
+		int i,j=0;
+		outer:for (i =0; i<size-1; i++) 
+		{
+			for  (j= i+1; j<size; j++)
+			{
+				if (array[j]== array[i])
+				{
+					isDuplicate= true; break outer;
+				}
+			}
+		}	
+	       if (isDuplicate)
+			System.out.println("array conatains duplicate elements at  
+                    position " + (i+1) + " and at " +  (j+1));
+		else
+			System.out.println("array doesn't contains duplicate 
+                    elements ");
 	}
 
+		       }
